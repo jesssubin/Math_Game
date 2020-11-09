a0a515dff860c273bcae77c8dd47cf3c694096b9
jesssubin <jess.s.moon@gmail.com>
2020-11-09 16:34:29 +0000

    Planning added

---
+++ plan.rb
@@ -0,0 +1,13 @@
+Classes 
+
+player 
+ - player 1 and 2 lives & names to be initialized (3/3)
+ - decrement lives when answer is wrong 
+
+question 
+ - providing randome questions 
+ - conditional verification if answer is true or wrong 
+
+game_turn 
+ - new turn or game over msg depending on lives of players
+ - swapping turns (current_player)
